6161028b03e495344f4ca717282c9c3e03855af0
AgzamxodjayevBilolxoja <agzamxodjayevbilolxoja@gmail.com>
2025-03-27 20:20:05 +0500

    Add User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class Gender(models.TextChoices):
+    MALE = ('male', 'Male')
+    FEMALE = ('female', 'Female')
+    NOSELECT = ('no_select', 'No Select')
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True, 
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])]
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        default=Gender.NOSELECT
+    )
